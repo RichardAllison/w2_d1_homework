02c1376b4e8b8d8e9e6dbc4caed538433c4c1400
Richard Allison <richard.allison.18@outlook.com>
2017-11-27 20:24:34 +0000

    created SportsTeam class, initialize method and team_name method, first test passed

--- sports_team/specs/sports_team_spec.rb
+++ sports_team/specs/sports_team_spec.rb
@@ -1,18 +1,23 @@
 require('minitest/autorun')
 require('minitest/rg')
-require_relative('../codeclan_student')
+require_relative('../sports_team')
 
 class TestSportsTeam < MiniTest::Test
 
-#   Now we would like you to make a class that represents a sports team.
-#
-# Make a class to represent a Team that has the properties Team name (String), Players (Array of strings) and a Coach (String).
-# For each property in the class make a Getter method than can return them.
-# Create a setter method to allow the coach's name to be updated.
-# Refactor the class to use attr_reader or attr_accessor instead of your own getter and setter methods.
-# Create a method that adds a new player to the player's array.
-# Add a method that takes in a string of a player's name and checks to see if they are in the players array.
-# Add a points property into your class that starts at 0.
-# Create a method that takes in whether the team has won or lost and updates the points property for a win.
+  #   Now we would like you to make a class that represents a sports team.
+  #
+  # Make a class to represent a Team that has the properties Team name (String), Players (Array of strings) and a Coach (String).
+  # For each property in the class make a Getter method than can return them.
+  # Create a setter method to allow the coach's name to be updated.
+  # Refactor the class to use attr_reader or attr_accessor instead of your own getter and setter methods.
+  # Create a method that adds a new player to the player's array.
+  # Add a method that takes in a string of a player's name and checks to see if they are in the players array.
+  # Add a points property into your class that starts at 0.
+  # Create a method that takes in whether the team has won or lost and updates the points property for a win.
+
+  def test_team_name
+    team = SportsTeam.new("Avengers FC", ["Tony Stark", "Bruce Banner", "Peter Parker", "Steve Rogers"], "Nick Fury")
+    assert_equal("Avengers FC", team.team_name)
+  end
 
 end
